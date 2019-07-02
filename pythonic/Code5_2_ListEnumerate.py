4cc90194895c46f1edd55ba84f8cae816a253524
mmunsi2 <masia.munsi@student.unamur.be>
2019-07-02 21:31:35 +0200

    Delete Code5_2_ListEnumerate.py
    File will be renamed

--- pythonic/Code5_2_ListEnumerate.py
+++
@@ -1,4 +0,0 @@
-names = ['Peter Parker', 'Clark Kent', 'Wade Wilson', 'Bruce Wayne']
-heroes = ['Spiderman', 'Superman', 'Deadpool', 'Batman']
-for name, hero in zip(names, heroes):
-    print(f'{name} is actually {hero}')
