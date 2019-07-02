36a8deedae0abe40cea3068a59c73f22fe7a7b5f
mmunsi2 <masia.munsi@student.unamur.be>
2019-07-02 21:31:39 +0200

    Delete Code5_1_ListEnumerate.py
    File will be renamed

--- pythonic/Code5_1_ListEnumerate.py
+++
@@ -1,5 +0,0 @@
-names = ['Peter Parker', 'Clark Kent', 'Wade Wilson', 'Bruce Wayne']
-heroes = ['Spiderman', 'Superman', 'Deadpool', 'Batman']
-for index, name in enumerate(names):
-    hero = heroes[index]
-    print(f'{name} is actually {hero}')
