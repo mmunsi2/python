1b857fe9a3efe5125f1b456227b45fb736c00af6
mmunsi2 <masia.munsi@student.unamur.be>
2019-07-21 16:27:54 +0200

    Update Code03_102_ContextManager.py
    Words replaced by Word

--- pythonic/Code03_102_ContextManager.py
+++ pythonic/Code03_102_ContextManager.py
@@ -1,6 +1,6 @@
 t = open('test.txt', 'r')
 file_contents = t.read()
 t.close()l I
-mots = file_contents.split(' ')
-mot_count = len(mots)
-print(mot_count)
+word = file_contents.split(' ')
+word_count = len(word)
+print(word_count)
