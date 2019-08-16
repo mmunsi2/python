be871a748f79d3b257c02cc3a72e0c37c2808511
mmunsi2 <masia.munsi@student.unamur.be>
2019-08-16 12:20:14 +0200

    Update Code09_1_MultByMod2.py

--- pythonic/Code09_1_MultByMod2.py
+++ pythonic/Code09_1_MultByMod2.py
@@ -1,8 +1,5 @@
 x=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 result = []
 for idx in range(len(x)):
-	if x[idx]%2==0:
-		result.append(x[idx] * 2)
-else:
-	result.append(x[idx])
-#print(result)
+    if x[idx]%2==0:
+        print((x[idx] * 2))
